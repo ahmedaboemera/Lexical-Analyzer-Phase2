1f605ffbb0124905eb45e47ef18a104261d818eb
omarbarakat <omarbarakat_92@yahoo.com>
2015-04-13 15:31:57 +0200

    utilities class

---
+++ src/Utilities.h
@@ -0,0 +1,27 @@
+/*
+ * Parser.h
+ *
+ *  Created on: 12 Apr 2015
+ *  Author: omarbarakat
+ */
+
+#ifndef UTILITIES_H_
+#define UTILITIES_H_
+
+#include <vector>
+#include <map>
+
+using namespace std;
+
+
+class Utilities{
+	public:
+		map<string, vector<string>> compute_first(vector<string> non_terminal);
+		map<string, vector<string>> compute_follow(vector<vector<vector<string> > > non_terminal_defs);
+	private:
+		vector<string>::iterator vector_contains(vector<string> vec, string str);
+		vector<string> get_first(string non_terminal);
+};
+
+
+#endif /* UTILITIES_H_ */
